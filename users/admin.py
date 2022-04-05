75f548d8f0f648a72b5efde8806f226eed85039f
advatrix <trojan.advatrix@gmail.com>
2022-04-05 21:46:45 +0300

    notifications

--- users/admin.py
+++ users/admin.py
@@ -1,17 +1,97 @@
+import django.forms.models
 from django.contrib import admin
+from django.db import connection
 
 from users.models import *
 
 # Register your models here.
 
 
+@admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     search_fields = ['username']
 
 
+@admin.register(Session)
 class SessionAdmin(admin.ModelAdmin):
     search_fields = ['username']
 
 
-admin.site.register(User, UserAdmin)
-admin.site.register(Session, SessionAdmin)
+@admin.register(Role)
+class RoleAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+
+@admin.register(UserToRole)
+class UserToRoleAdmin(admin.ModelAdmin):
+    search_fields = ['user__username', 'role__name']
+    autocomplete_fields = ['user']
+
+
+@admin.register(TargetGroup)
+class TargetGroupAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+
+@admin.register(UserToTargetGroup)
+class UserToTargetGroup(admin.ModelAdmin):
+    search_fields = ['target_group__name', 'user__username']
+    autocomplete_fields = ['user']
+    list_filter = ['target_group__name']
+
+
+@admin.register(NotificationToUser)
+class NotificationToUserAdmin(admin.ModelAdmin):
+    search_fields = ['notification__title', 'user__username']
+    autocomplete_fields = ['user']
+
+
+class NotificationToTargetGroupInlineFormset(django.forms.models.BaseInlineFormSet):
+    def save_new_objects(self, commit=True):
+        saved_objects = super(NotificationToTargetGroupInlineFormset, self).save_new_objects(commit)
+        if commit:
+            for nttg in saved_objects:
+                query = f'''
+                    insert into "notification_to_user" (
+                        is_read,
+                        created,
+                        updated,
+                        notification_id,
+                        user_id
+                    )
+                    select
+                        false,
+                        now(),
+                        now(),
+                        {nttg.notification.id},
+                        user_id
+                    from user_to_target_group uttg
+                    where target_group_id = {nttg.target_group.id}
+                    on conflict do nothing
+                '''
+
+                with connection.cursor() as cursor:
+                    cursor.execute(query)
+        return saved_objects
+
+    def delete_existing(self, obj, commit=True):
+        if commit:
+            NotificationToUser.objects.filter(
+                notification=obj.notification,
+                user_id__in=UserToTargetGroup.objects.filter(target_group=obj.target_group).values_list('user_id',
+                                                                                                         flat=True)
+            ).delete()
+        super(NotificationToTargetGroupInlineFormset, self).delete_existing(obj, commit)
+
+
+class NotificationToTargetGroupInline(admin.TabularInline):
+    model = NotificationToTargetGroup
+    formset = NotificationToTargetGroupInlineFormset
+
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    search_fields = ['title']
+    inlines = [NotificationToTargetGroupInline]
+
+
